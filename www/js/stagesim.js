12aaf3e66d8ea268ed63cc5bc63e6ab5a56a528a
Enric Cervera <ecervera@uji.es>
2014-02-13 16:14:31 +0100

    stage grid

--- www/js/stagesim.js
+++ www/js/stagesim.js
@@ -35,6 +35,25 @@ var StageSim = (function() {
 		var ratio = bh/bw;
 		this.context.drawImage(this.background,0,cw*(1-ratio)/2,cw,cw*ratio);
 
+		if($("#dispGrid").is(':checked')){
+			var scale = cw/bw;
+			var mpix = this.mpix/scale;
+			var n = 6; // grid lines from -n to n
+			for (var x=-n;x<=n;x++) {
+				this.context.beginPath();
+				this.context.moveTo(x/mpix+cw/2,-n/mpix+cw/2);
+				this.context.lineTo(x/mpix+cw/2, n/mpix+cw/2);
+				this.context.strokeStyle = 'rgba(192,192,192,0.5)';
+				this.context.stroke();
+			}
+			for (var y=-n;y<=n;y++) {
+				this.context.beginPath();
+				this.context.moveTo(-n/mpix+cw/2, y/mpix+cw/2);
+				this.context.lineTo( n/mpix+cw/2, y/mpix+cw/2);
+				this.context.strokeStyle = 'rgba(192,192,192,0.5)';
+				this.context.stroke();
+			}
+		}
  		//this.bot.draw();
 		for (var i=0;i<this.bot.length;i++) {
 			this.bot[i].draw();
